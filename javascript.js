50f2fcca0a91fc0324779c21cc457adfdfd056db
Kasey <kasey.murdoch@yahoo.com>
2022-03-19 22:10:31 -0700

    Two players rotate playing

--- javascript.js
+++ javascript.js
@@ -1,25 +1,53 @@
 const container = document.querySelector(`.container`)
 
-const gameboard = (() => {
-    const array = [`X`, `X`, `O`, `X`, `X`, `O`, `O`, `X`, `O`]
+const personFactory = (name, symbol) => {
     return {
-        array
+        name,
+        symbol
+    }
+}
+
+const playerOne = personFactory(`Player 1`, `X`)
+const playerTwo = personFactory(`Player 2`, `O`)
+let activePlayer = playerOne
+
+const gameboard = (() => {
+    const array = [``, ``, ``, ``, ``, ``, ``, ``, ``]
+    const drawBoard = () => {
+        container.innerHTML = ``
+        gameboard.array.forEach((e, index) => {
+            const box = document.createElement(`button`)
+            box.setAttribute(`data-index`, index)
+            box.classList.add(`box`)
+            box.textContent = `${e}`
+            container.appendChild(box)
+        })
+    }
+    const toggleBox = (event) => {
+        if (event.target.className === `box` && event.target.innerHTML === ``) {
+            const index = event.target.getAttribute(`data-index`)
+            gameboard.array.splice(index, 1, activePlayer.symbol)
+            if (activePlayer === playerOne) {
+                activePlayer = playerTwo
+            }
+            else {
+                activePlayer = playerOne
+            }
+        console.log(activePlayer)
+        }
+        gameboard.drawBoard()
+
+    }
+
+    return {
+        array,
+        drawBoard,
+        toggleBox
     }
 })()
+gameboard.drawBoard()
 
-const personFactory = (name) => {
-    return {
-        name
-    }
-}
 
-const drawBoard = () => {
-    gameboard.array.forEach((e, index) => {
-        const box = document.createElement(`div`)
-        box.setAttribute(`data-index`, index)
-        box.textContent = `${e}`
-        container.appendChild(box)
-    })
-}
-
-drawBoard()
+document.addEventListener(`click`, (event) => {
+    gameboard.toggleBox(event)
+})
