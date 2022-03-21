6dbfc9f4fd327f67d675177cb2b9be40f04354fa
Kasey <kasey.murdoch@yahoo.com>
2022-03-20 20:42:31 -0700

    New game button added to replay more

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(`.container`)
 const info = document.querySelector(`.info`)
+const start = document.querySelector(`.start`)
 
 const personFactory = (name, symbol) => {
     return {
@@ -38,7 +39,6 @@ const gameboard = (() => {
         else {
             return
         }
-        console.log(gameboard.array)
         gameboard.drawBoard()
 
     }
@@ -55,22 +55,50 @@ const game = (() => {
     const winCombo = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     const checkWin = (arr) => {
         // Iterates through winCombo and on each iteration we will use the arrays that are inside winCombo
-        arr.forEach((combo) => {
-            // Uses every number (0,1,2 ... 3,4,5 ... 6,7,8) inside of the arrays (combo) of winCombo as indexes for gameboard.array (i.e. checks gameboard.array index 0, 1 and 2 to see if they all contain either an X or a O)
-            if (combo.every(element => gameboard.array[element] === playerOne.symbol)) {
+        arr.forEach((subArr) => {
+            // Uses every number (0,1,2 ... 3,4,5 ... 6,7,8) inside of the arrays (subArr) of winCombo as indexes for gameboard.array (i.e. checks gameboard.array index 0, 1 and 2 to see if they all contain either an X or a O)
+            if (subArr.every(element => gameboard.array[element] === playerOne.symbol)) {
                 console.log(`player 1 wins`)
+                game.disableButtons()
             }
-            if (combo.every(element => gameboard.array[element] === playerTwo.symbol)) {
+            if (subArr.every(element => gameboard.array[element] === playerTwo.symbol)) {
                 console.log(`player 2 wins`)
+                game.disableButtons()
             }
         })
+    }
+    const disableButtons = () => {
+        const buttons = container.querySelectorAll(`button`)
+        buttons.forEach((e) => {
+            e.disabled = true
+        })
+    }
+    const enableButtons = () => {
+        const buttons = container.querySelectorAll(`button`)
+        buttons.forEach((e) => {
+            e.disabled = false
+        })
+    }
+    const startNewGame = () => {
+        gameboard.array = [``, ``, ``, ``, ``, ``, ``, ``, ``]
+        activePlayer = playerOne
+        gameboard.drawBoard()
+        game.enableButtons()
     }
 
     return {
         winCombo,
-        checkWin
+        checkWin,
+        disableButtons,
+        enableButtons,
+        startNewGame
     }
 })()
+game.disableButtons()
+
+start.addEventListener(`click`, () => {
+    game.startNewGame()
+})
 
 container.addEventListener(`click`, (event) => {
     if (event.target.className === `box` && event.target.innerHTML === ``) {
